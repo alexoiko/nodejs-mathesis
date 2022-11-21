22464379e390fbeaf0851fd74d20c216923dc05b
Alexandros Oikonomou <alex@wifi246-023.pubaccess.auth.gr>
2022-11-21 19:44:24 +0200

    second commit

--- model/config.mjs
+++ model/config.mjs
@@ -1,19 +1,34 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-    {
-        host: 'localhost',
-        port: 5432,
-        database: 'postgres',
-        dialect: 'postgres',
-        username: 'postgres',
-        password: '12345',
-        database: 'bookDB',
-        logging: false,
-        define: {
-            timestamps: false,
-            freezeTableName: true
-        }
-    });
+// const sequelize = new Sequelize(
+//     {
+//         host: 'localhost',
+//         port: 5432,
+//         database: 'postgres',
+//         dialect: 'postgres',
+//         username: 'postgres',
+//         password: '12345',
+//         database: 'bookDB',
+//         logging: false,
+//         define: {
+//             timestamps: false,
+//             freezeTableName: true
+//         }
+//     });
 
-export default sequelize
+const sequalizer = new Sequelize(process.env.DATABASE_URL,{
+        dialect: 'postgres',
+        dialectOptions:{
+
+            ssl:{
+                require: true,
+                rejectUnauthorized: false
+            },
+            logging: false, 
+            define:{
+                timestamps: false
+            }
+        }
+})
+
+export default sequelizer
